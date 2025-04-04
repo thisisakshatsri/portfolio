e8ddfdc4d371c7921954c4e8fdf10320a8ec396a
Akshat Srivastava <akshat.srivastava643@gmail.com>
2025-04-04 20:43:55 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <p className="text-sm text-muted-foreground">
-              &copy; {currentYear} Akshat Srivastava. All rights reserved.
+              &copy; {currentYear} | Designed with ❤️ by Akshat.
             </p>
           </div>
           
